eb8324f61f418a6db33f61bebe59425a593473ad
morinekiyohisa <64891242+morinekiyohisa@users.noreply.github.com>
2020-05-11 10:06:27 +0800

    Update main.py
    *改用函數計算
    *文字表達待修正

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+def calup(price):
+    up=(price*0.1)+price
+    return up
+
+def caldown(price):
+    down=price-(price*0.1)
+    return down
 price= float(input("請輸入昨日收盤價"))
-print("1.漲停價")
-print("2.跌停價")
-slect = int(input("要算漲停價還是跌停價"))
-if slect == 1:
-  up=(price*0.1)+price
-  print(up)
-elif slect == 2:
-  down=price-(price*0.1)
-  print(down)
-else:
-  print("參數無效")
+up = calup(price)
+down = caldown(price)
+print(up)
+print(down)
