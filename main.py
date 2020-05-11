5b792476020fce7ee11bcfe09b29ba2cbfbc6e74
morinekiyohisa <64891242+morinekiyohisa@users.noreply.github.com>
2020-05-11 19:39:25 +0800

    Update main.py
    *更新注釋

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-def calup(price):
+#<------------------------------------------------------->
+def calup(price):#漲停價計算函數
     up=(price*0.1)+price
     return up
-
-def caldown(price):
+#<------------------------------------------------------->
+def caldown(price):#跌停價計算函數
     down=price-(price*0.1)
     return down
+#<------------------------------------------------------->
 price= float(input("請輸入昨日收盤價"))
-up = calup(price)
-down = caldown(price)
-print(up)
-print(down)
+up = calup(price)#調用calup
+down = caldown(price)#調用caldown
+print('漲停價',up)
+print('跌停價',down)
+#<------------------------------------------------------->
